7c088047efc67c40a9bf208595c6fab2030bd658
Paul Cantalupo <pcantalupo@gmail.com>
2017-01-31 16:14:40 -0500

    cannot calculate nsf on a protein sequence

--- SeqUtils.pm
+++ SeqUtils.pm
@@ -23,17 +23,18 @@ sub has_nsf {
     $seq = Bio::Seq->new(-seq => $seq, -id => "foobar");
   }
 
-   my @orfs_6frames = Bio::SeqUtils->translate_6frames($seq);
-
-   my $toReturn = 0;
-   foreach my $orfseq (@orfs_6frames) {
-   # search for at least one frame that does not have a stop codon '*'
+  my $toReturn = 0;
+  if ($seq->alphabet eq 'dna') {
+    my @orfs_6frames = Bio::SeqUtils->translate_6frames($seq);
+    foreach my $orfseq (@orfs_6frames) {
+      # search for at least one frame that does not have a stop codon '*'
       if ($orfseq->seq !~ /\*/) {   # if you don't find an '*', the sequence contains an ORF throughout entire frame
-         $toReturn = 1;
+        $toReturn = 1;
       }
-   }
+    }
+  }
 
-   return $toReturn;
+  return $toReturn;
 }
 
 
