92756e82c3426d832f2ab1aa19168583d2e6c1bb
Paul Cantalupo <pcantalupo@gmail.com>
2022-07-21 16:22:28 -0400

    changing environment variable names for Names and Nodes

--- bin/acc2lineage_wrapper.sh
+++ bin/acc2lineage_wrapper.sh
@@ -2,8 +2,8 @@
 
 # These environment variables must be set:
 # 1. TAXASQL - path to the accessionTaxa.sql database (taxonomizr R package)
-# 2. TAXASQL_NAMESDMP - path to the names.dmp taxonomy file
-# 3. TAXASQL_NODESDMP - path to the nodes.dmp taxonomy file
+# 2. NAMESDMP - path to the names.dmp taxonomy file
+# 3. NODESDMP - path to the nodes.dmp taxonomy file
 
 set -euo pipefail
 module purge && module load gcc/10.2.0 r/4.2.0
@@ -11,8 +11,8 @@ module purge && module load gcc/10.2.0 r/4.2.0
 acc=$1
 
 ln -sf $TAXASQL
-ln -sf $TAXASQL_NAMESDMP
-ln -sf $TAXASQL_NODESDMP
+ln -sf $NAMESDMP
+ln -sf $NODESDMP
 
 acc2lineage.R $TAXASQL "$acc"
 
